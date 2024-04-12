956f0f431552d87ba562b50f02c865d14b34922f
vinkel555 <snowdon03@gmail.com>
2024-04-12 11:17:26 +0100

    Filename updates

---
+++ cloudflare-provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  #Credentials Imported from $CLOUDFLARE_API_TOKEN environment variable
+}
+
